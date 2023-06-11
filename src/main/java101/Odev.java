50b6f3cd63e5761ab23402d204bf563bfdb14556
harunyyilmaz <harun412451@gmail.com>
2023-06-11 12:25:12 +0300

    java101

---
+++ src/main/java101/Odev.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+
+public class Odev {
+    public static void main(String[] args) {
+
+        Scanner input=new Scanner(System.in);
+
+        double kg,m;
+        double kiloIndeks;
+
+        System.out.print("Kilonuzu Giriniz:");
+        kg= input.nextDouble();
+
+        System.out.print("Boyunuzu Giriniz:");
+        m= input.nextDouble();
+
+        System.out.print("Kilo İndeksi:");
+        kiloIndeks= (kg/m)*m;
+        System.out.println(kiloIndeks);
+
+
+
+
+
+    }
+}
