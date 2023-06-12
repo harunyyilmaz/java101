eb2bcb3b7e67f007fc9ebf9d1b3d0a31ed8ff4ec
harunyyilmaz <harun412451@gmail.com>
2023-06-12 15:07:18 +0300

    java101

---
+++ src/main/java101/HesapMakinesi2.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class HesapMakinesi2 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        double x1,x2,x3,Select;
+
+        System.out.print("ilk sayıyı giriniz:");
+        x1= input.nextDouble();
+        System.out.print("ikinci sayıyı giriniz:");
+        x2= input.nextDouble();
+        System.out.print("üçüncü sayıyı giriniz:");
+        x3= input.nextDouble();
+
+        System.out.println("1-toplama\n2-çıkarma\n3-çarpma\n4-bölme");
+        System.out.print("Seçiniz:");
+        Select= input.nextDouble();
+
+
+
+        if (Select==1) {
+            System.out.println("toplama:"+ (x1+x2+x3));
+        }else if(Select==2){
+            System.out.println("çıkarma:"+(x1-x2-x2));
+        }else if(Select==3){
+            System.out.println("çarpma:"+(x1*x2*x3));
+        }else if(Select==4){
+            if(x2!=0){
+
+            System.out.println("bölme:"+(x1/x2/x3));
+        }else{
+                System.out.println("\"Bir sayı sıfır ile bölünemez\"");
+            }
+            }else{
+                System.out.println("Hatalı seçim yaptnız: Tekrar deneyiniz");
+            }
+    }
+}
---
+++ src/main/java101/IfElseSoru.java
@@ -0,0 +1,15 @@
+public class IfElseSoru {
+    public static void main(String[] args) {
+
+        int a = 20, b = 10;
+        if ((a < b) && (b++ < 25)){
+            System.out.println(a);
+        }else{
+            System.out.println(a);
+        }
+        System.out.println(b);
+
+
+
+    }
+}
