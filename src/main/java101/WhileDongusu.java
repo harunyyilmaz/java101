03412bd52d8e6baf2a58793888d51c4dbfc3d151
harunyyilmaz <harun412451@gmail.com>
2023-06-19 12:31:20 +0300

    java101

---
+++ src/main/java101/WhileDongusu.java
@@ -0,0 +1,20 @@
+package main.java101;
+
+import java.util.Scanner;
+
+public class WhileDongusu {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int k;
+        System.out.print("Sayıyı giriniz:");
+        k= input.nextInt();
+
+        for(int i=0; i<=k; ++i){
+            if(i%3==0 && i%4==0)
+            System.out.println(i);
+        }
+
+    }
+
+}
