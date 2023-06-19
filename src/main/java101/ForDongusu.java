45480700176442a51c3f389c46874dad4e68a952
harunyyilmaz <harun412451@gmail.com>
2023-06-19 13:47:10 +0300

    java101

--- src/main/java101/ForDongusu.java
+++ src/main/java101/ForDongusu.java
@@ -7,14 +7,24 @@ public class ForDongusu {
 
         Scanner input = new Scanner(System.in);
         int k;
+        int total=0 , adet=0;
+        double ort;
         System.out.print("Sayıyı giriniz:");
         k= input.nextInt();
 
-        for(int i=0; i<=k; ++i){
-            if(i%3==0 && i%4==0)
-            System.out.println(i);
-        }
+        for(int i=0; i<=k; ++i) {
+            if (i % 3 == 0 && i % 4 == 0) {
+                System.out.println(i);
+                total = total + i;
+                adet++;
+            }
 
-    }
+        }
+        System.out.println("Sayıların toplamı:"+total);
+        System.out.println("Sayı adeti:"+adet);
+        ort=total/adet;
+        System.out.println("Sayıların ortalması:"+ort);
+
+        }
 
 }
