66468a59a0e69d6a0400a440fa08dee6fa30edc8
harunyyilmaz <harun412451@gmail.com>
2023-06-25 07:36:30 +0300

    java101

---
+++ src/main/java101/YildizlarlaElmas.java
@@ -0,0 +1,33 @@
+package main.java101;
+
+import java.util.Scanner;
+
+public class YildizlarlaElmas {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Basamak sayısnı giriniz:");
+        int n= input.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int k = 1; k <= n- i; k++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+        for(int i=1; i<n; i++) {
+            for (int k = 1; k <= i - 1; k++) {
+                System.out.print(" ");
+            }
+            for( int j=1; j<=2*n-1; j++){
+                System.out.print("*");
+            }
+        }
+    }
+}
